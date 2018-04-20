1e3df08b82ed4d32964e7ca552ad5dcfb123eae6
waaah <cojosh90@gmail.com>
2018-04-20 23:48:58 +0800

    Added the Elections Project

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+//use static first before doing any configuring of routers.
+app.use(express.static(path.join(__dirname, 'views')));
+app.set('view engine', 'ejs');
+
+//express routers.
+const main = require('./routes/main'); 
+app.use('', main);
+//end of express router
+
+port = process.env.port || 3000;
+app.listen(port, function(){
+    console.log("connected to port");
+});
